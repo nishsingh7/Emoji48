3f0166fe322367db1eee0ab65842a422dcdd7979
Nish Singh <nish7@live.co.uk>
2019-09-21 16:00:56 +0100

    MVP of expression classification (not so great at classification just yet)

--- Face Gesture/ViewController.swift
+++ Face Gesture/ViewController.swift
@@ -13,63 +13,101 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    enum Expression: String {
+        case tongueOut
+        case smiling
+        case puffedCheeks
+        case eyeRoll
+        case mouthOpen
+        case crossedEyed
+        case leftWink
+        case rightWink
+        case noseSneer
+        case mouthKiss
+        case none
+    }
+    
+    var currentState: Expression = .none { willSet { if newValue != self.currentState {
+        DispatchQueue.main.async { self.outputLabel.text = newValue.rawValue }}}}
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        let configuration = ARFaceTrackingConfiguration()
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
 
     // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
+        let faceMesh = ARSCNFaceGeometry(device: sceneView.device!)
+        let node = SCNNode(geometry: faceMesh)
+        node.geometry?.firstMaterial?.fillMode = .lines
         return node
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+    func session(_ session: ARSession, didFailWithError error: Error) { }
+    
+    func sessionWasInterrupted(_ session: ARSession) { }
+    
+    func sessionInterruptionEnded(_ session: ARSession) { }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let faceAnchor = anchor as? ARFaceAnchor, let faceGeometry = node.geometry as? ARSCNFaceGeometry {
+            faceGeometry.update(from: faceAnchor.geometry)
+            extractExpression(anchor: faceAnchor)
+        }
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
+    func extractExpression(anchor: ARFaceAnchor) {
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        let smileLeft = anchor.blendShapes[.mouthSmileLeft]
+        let smileRight = anchor.blendShapes[.mouthSmileRight]
+        let cheekPuff = anchor.blendShapes[.cheekPuff]
+        let tongue = anchor.blendShapes[.tongueOut]
+        let leftEyeUp = anchor.blendShapes[.eyeLookUpLeft]
+        let rightEyeUp = anchor.blendShapes[.eyeLookUpRight]
+        let mouthOpen = anchor.blendShapes[.mouthFunnel]
+        let leftEyeIn = anchor.blendShapes[.eyeLookInLeft]
+        let rightEyeIn = anchor.blendShapes[.eyeLookInRight]
+        let leftWink = anchor.blendShapes[.eyeBlinkLeft]
+        let rightWink = anchor.blendShapes[.eyeBlinkRight]
+        let noseSneerLeft = anchor.blendShapes[.noseSneerLeft]
+        let noseSneerRight = anchor.blendShapes[.noseSneerRight]
+        let mouthKiss = anchor.blendShapes[.mouthPucker]
         
+        if ((smileLeft?.decimalValue ?? 0.0) + (smileRight?.decimalValue ?? 0.0)) > 0.9 {
+            currentState = .smiling
+        } else if cheekPuff?.decimalValue ?? 0.0 > 0.3 {
+            currentState = .puffedCheeks
+        } else if tongue?.decimalValue ?? 0.0 > 0.1 {
+            currentState = .tongueOut
+        } else if ((leftEyeUp?.decimalValue ?? 0.0) + (rightEyeUp?.decimalValue ?? 0.0)) > 0.9  {
+            currentState = .eyeRoll
+        } else if mouthOpen?.decimalValue ?? 0.0 > 0.3 {
+            currentState = .mouthOpen
+        } else if ((leftEyeIn?.decimalValue ?? 0.0) + (rightEyeIn?.decimalValue ?? 0.0)) > 0.9 {
+            currentState = .crossedEyed
+        } else if leftWink?.decimalValue ?? 0.0 > 0.1 {
+            currentState = .leftWink
+        } else if rightWink?.decimalValue ?? 0.0 > 0.1 {
+            currentState = .rightWink
+        } else if ((noseSneerLeft?.decimalValue ?? 0.0) + (noseSneerRight?.decimalValue ?? 0.0)) > 0.9 {
+            currentState = .noseSneer
+        } else if mouthKiss?.decimalValue ?? 0.0 > 0.1 {
+            currentState = .mouthKiss
+        } else {
+            currentState = .none
+        }
     }
 }
