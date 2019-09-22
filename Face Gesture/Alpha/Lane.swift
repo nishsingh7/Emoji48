317afd92b9aacc8b18c9c30ffa641848cd6c5041
Mike Cooper <mc14641@my.bristol.ac.uk>
2019-09-22 14:02:54 +0100

    This is a descriptive commit message.

--- Face Gesture/Alpha/Lane.swift
+++ Face Gesture/Alpha/Lane.swift
@@ -55,7 +55,7 @@ class Lane: SCNNode {
     }
     
     private func generateDisc() {
-        let
+//        let
     }
 }
 
--- Face Gesture/Alpha/MasterVC.swift
+++ Face Gesture/Alpha/MasterVC.swift
@@ -14,12 +14,20 @@ class MasterVC: UIViewController {
 
 
     // Injection parameters
-    let expressions: [Expression] = [.crazy, .money, .wink]
+    let expressions: [Expression] = [.crazy, .money, .wink, .kiss]
     let song: Song = .knightrider
     let difficult: Difficulty = .easy
 
     // Components
-    @IBOutlet var gameScene: GameScene?
+    @IBOutlet weak var sceneView: SCNView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var percentageLabel: UILabel!
+    @IBOutlet weak var closeButton: UIButton!
+    
+    var gameScene: GameScene?
+    
+//    var coinClassification: [CoinScoreClassification]
+    
     
     private lazy var classifier: Classifier = {
         let object = Classifier(parent: self)
@@ -29,18 +37,32 @@ class MasterVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setUpUI()
         let script: SongScript = Butler.generateSongScript(forSong: .knightrider, difficulty: .easy)!
-        self.gameScene = GameScene(expressions: [], script: script, delegate: self)
+        self.gameScene = GameScene(expressions: expressions, script: script, delegate: self)
+        sceneView.scene = gameScene
+        sceneView.allowsCameraControl = true
         classifier.huntForExpression(expressions)
     }
     
+    private func setUpUI() {
+        scoreLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        scoreLabel.backgroundColor = #colorLiteral(red: 0, green: 0.6714670062, blue: 0.6813778281, alpha: 0.5781525088)
+        scoreLabel.clipsToBounds = true
+        scoreLabel.layer.borderWidth = 3.0
+        scoreLabel.layer.borderColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
+        scoreLabel.layer.cornerRadius = 20
+    }
+    
     private func detectedExpression(_ expressions: [Expression]) {
-        
+        gameScene?.updatedExpression(expressions)
     }
 }
 
 extension MasterVC : GameSceneDelegate {
     func didClassifyCoin(laneIndex: Int, classification: CoinScoreClassification) {
         print("Coin Classified")
+//        coinClassificationp[laneIndex]
     }
 }
+
